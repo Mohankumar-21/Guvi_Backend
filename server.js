a02b057863b23b77988516f36533b90aeca5b21b
Mohankumar@LAPTOP-DSRRQ3HB <gmk9155@gmail.com>
2024-01-26 18:36:23 +0530

    first commit

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ connectDB();
 app.use(express.json());
 app.use(morgan('dev'));
 
-  
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 //routes
 app.use('/api/v1/user', require('./routes/userRouter'));
 app.use('/api/v1/user', require('./routes/profileRoute'));
